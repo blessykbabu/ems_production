edb9e1369d4f1eb6753d4d899b3df7b87ab75c98
blessykbabu <blessykbabu5@gmail.com>
2024-01-21 23:25:20 +0530

    change

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/public'
+    outDir: '../server/public',
+    emptyOutDir:true
   }
+
 })
